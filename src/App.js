6655d498438cdcb47efe08c5bad2688654d779a0
romank123 <67678390+romank123@users.noreply.github.com>
2022-09-13 00:25:45 +0300

    fixed homework-2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [messageList, setMessageList] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const target = event.target;
+    const author = target.author.value;
+    const text = target.text.value;
+
+    setMessageList((prevState) => [
+      ...prevState,
+      {
+        id: giveLastId(prevState),
+        author: author,
+        text: text,
+      },
+    ]);
+  };
+
+  function giveLastId(arr) {
+    return arr.length ? arr[arr.length - 1].id + 1 : 0;
+  }
+
+  function botAnswer() {
+    const lastAuthor = messageList[messageList.length - 1];
+    if (lastAuthor && lastAuthor.author) {
+      setMessageList((prevState) => [
+        ...prevState,
+        {
+          id: giveLastId(prevState),
+
+          text: `Сообщение автора ${lastAuthor.author} отправлено`,
+        },
+      ]);
+    }
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      botAnswer();
+    }, 2000);
+  }, [messageList]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type='text' name='author'></input>
+        <input type='text' name='text'></input>
+        <input type='submit' value='отправить'></input>
+      </form>
+      {messageList.map((message) => {
+        return (
+          <div key={message.id}>
+            {message.author && (
+              <p>
+                <span>Автор:</span>
+                {message.author}
+              </p>
+            )}
+            <p>
+              {message.author && <span></span>} {message.text}
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 }
